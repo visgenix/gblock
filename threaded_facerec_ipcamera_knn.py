6388fe836f4b8fc932bc297c52a05aea6c24c761
visgenix <153745118+visgenix@users.noreply.github.com>
2024-01-06 10:35:53 +0530

    time update

--- threaded_facerec_ipcamera_knn.py
+++ threaded_facerec_ipcamera_knn.py
@@ -225,7 +225,7 @@ def Train_Recognize(thresh):
         masked_frame = cv2.bitwise_and(frame,frame,mask=mask)
         #frame=cv2.rectangle(frame,(90,75),(400,300),(0,255,0),2)
         cv2.imshow('camera',masked_frame)
-        if get_time()=="10:15" or get_time()=="21:30":
+        if get_time()=="10:15" or get_time()=="20:55":
             print('time up')
             if get_time()=="10:15":
                 break
